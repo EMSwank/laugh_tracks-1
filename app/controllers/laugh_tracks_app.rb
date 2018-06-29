8f54cd103cbec79a59d12fb072e2da4a44f1e3c6
Eliot Swank <eliotswank@gmail.com>
2018-06-29 12:32:55 -0600

    finishes story 4

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,7 +1,12 @@
 class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
-    @comedians = Comedian.all.includes(:specials)
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age])
+                           .includes(:specials)
+    else
+      @comedians = Comedian.all.includes(:specials)
+    end
     @average_age = Comedian.average_age
     erb :'comedians/index'
   end
