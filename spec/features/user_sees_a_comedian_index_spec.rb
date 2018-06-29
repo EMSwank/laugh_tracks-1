47f32581fe33ff6544bfe9e562f3de35fb3b0a7e
Eliot Swank <eliotswank@gmail.com>
2018-06-29 10:31:55 -0600

    adds test for story 3

--- spec/features/user_sees_a_comedian_index_spec.rb
+++ spec/features/user_sees_a_comedian_index_spec.rb
@@ -9,13 +9,22 @@ describe "visitor" do
       expect(page).to have_content(comedian.age) 
     end
 
-    it "and sees a list of comedians with Name and Age" do
+    it "and sees a list of comedian specials" do
       comedian = Comedian.create(name: "Eddie Murphy", age: 55)
       special = Special.create(name: "Delirious", comedian_id: comedian.id)
 
       visit '/comedians'
 
       expect(page).to have_content(special.name) 
+    end
+
+    it "and sees the average age of all comedians" do
+      comedian1 = Comedian.create(name: "Eddie Murphy", age: 50)
+      comedian2 = Comedian.create(name: "Seth Rogan", age: 40)
+      average_age = 45
+      visit '/comedians'
+
+      expect(page).to have_content(average_age) 
     end
   end
 end
@@ -26,6 +35,6 @@ end
 =begin
 As a visitor,
 When I visit `/comedians`
-Then I also see a list of each comedian's specials.
+Then I also see the average age for all comedians.
 
 =end
